79bdd8bb24ca4d76ae70484e9c510f55ed0e4703
Thabiso Seleke <mrthabiso.seleke@gmail.com>
2024-03-10 16:16:18 +0200

    full code

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params:{
-        key: '0298e018766448afbf74f99d8a4a7ce7'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "c7b18323a47d40c394ea5b019646b1f5",
+  },
+});
