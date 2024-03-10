0ab521bc1388f30aa65f804af098566c6b3d7d27
Thabiso Seleke <mrthabiso.seleke@gmail.com>
2024-03-10 16:31:47 +0200

    api key update

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "0298e018766448afbf74f99d8a4a7ce7",
   },
 });
